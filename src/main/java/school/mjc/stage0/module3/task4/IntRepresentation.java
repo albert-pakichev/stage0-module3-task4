d5fab0722ef44d46a83ddb11ff854d4f9b2bd2d8
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-06-25 12:08:25 +0500

    Update IntRepresentation.java

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+         int first = 99;
+           int second = 104;
+           int third = 97;
+           int four = 114;
+           char t1;
+           char t2;
+           char t3;
+           char t4;
+           t1 = (char) first;
+           t2 = (char) second;
+           t3 = (char) third;
+           t4 = (char) four;
+           System.out.printf("%s%s%s%s", t1, t2, t3, t4);
     }
 }
